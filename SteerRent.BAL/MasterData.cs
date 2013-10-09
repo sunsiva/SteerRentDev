429e4ee6d7be9f72a6edd85005212e53916739c7
sunsiva <sivaprakasams@gmail.com>
2013-10-09 22:56:43 +0530

    Master data file for BAL added

---
+++ SteerRent.BAL/MasterData.cs
@@ -0,0 +1,21 @@
+﻿using SteerRent.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dal=SteerRent.DAL;
+
+namespace SteerRent.BAL
+{
+    public class MasterData
+    {
+
+        dal.MasterData objDAL;
+        public List<MasterDataModel> GetMasterData()
+        {
+            objDAL = new dal.MasterData();
+            return objDAL.GetMasterData();
+        }
+    }
+}
