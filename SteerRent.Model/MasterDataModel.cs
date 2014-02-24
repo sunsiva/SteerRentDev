135dc6128229ad30b40ff7f2b3ca1ac0e52533dc
sunsiva <sivaprakasams@gmail.com>
2014-02-24 09:12:59 +0530

    Company details updated

--- SteerRent.Model/MasterDataModel.cs
+++ SteerRent.Model/MasterDataModel.cs
@@ -89,16 +89,68 @@ namespace SteerRent.Model
     
     public class EmployeeModel
     {
-        public decimal LookupCategoryID { get; set; }
-        public string LookupCategoryCode { get; set; }
-        public string LookupCategoryDesc { get; set; }
-
-        public GlobalEnum.Flag ActionMode { get; set; }
-        public int UserId { get; set; }
+        public List<LocationModel> LocationList { get; set; }
+        public decimal EmployeeId { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string EmployeeCode { get; set; }
+        public Int16 Gender { get; set; }
+        public DateTime DOB { get; set; }
+        public decimal DesignationId { get; set; }
+        public string Designation { get; set; }
+        public decimal BUId { get; set; }
+        public string BU { get; set; }
+        public decimal LocationId { get; set; }
+        public string Location { get; set; }
+        public DateTime DOJ { get; set; }
+        public DateTime LeavingDate { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public decimal CountryId { get; set; }
+        public string CountryName { get; set; }
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContactPhone { get; set; }
         public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public GlobalEnum.Flag ActionMode { get; set; }
     }
 
     public class CompanySetup
+    {
+        public decimal OrgID { get; set; }
+        public string OrgCode { get; set; }
+        public string OrgLogoPath { get; set; }
+        public string OrgName { get; set; }
+        public string OrgAddress1 { get; set; }
+        public string OrgAddress2 { get; set; }
+        public string OrgAddress3 { get; set; }
+        public string OrgCity { get; set; }
+        public decimal OrgCountryId { get; set; }
+        public string OrgCountry { get; set; }
+        public decimal OrgEmirate { get; set; }
+        public string OrgEmirateName { get; set; }
+        public string OrgPostBoxNo { get; set; }
+        public string CPersonName { get; set; }
+        public string CEMailID { get; set; }
+        public string CMobileNo { get; set; }
+        public string CPersonDesignation { get; set; }
+        public string OrgZip { get; set; }
+        public string OrgPhoneNo { get; set; }
+        public string OrgFaxNo { get; set; }
+        public string OrgEmailID { get; set; }
+        public decimal BaseCurrencyId { get; set; }
+        public string BaseCurrency { get; set; }
+        public string DateFormat { get; set; }
+        public string UserId { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class BuSetup
     { 
         public decimal BuId { get; set; }
         public decimal OrgId { get; set; }
